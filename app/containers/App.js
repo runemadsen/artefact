10efcc9024cbb2151630392385c09e75654649a5
Martin Bravo <bravomartin@gmail.com>
2017-03-15 15:22:05 -0400

    fix logout

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import Helmet from 'react-helmet'
 import Navigation from '../components/Navigation'
-
+import { signOut } from '../actions/auth'
 class App extends Component {
   render() {
     return (
